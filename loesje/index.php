fb2089a6ab74a0463fa5542d560b1a140e8b9036
Dennis Paauw <dennis@thinkyellow.nl>
2023-06-26 09:00:08 +0200

    Loesje scherm

---
+++ loesje/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+    <head>
+		<meta charset="utf-8">
+		<title>Werkkracht - Loesje.nl</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<link rel="stylesheet" href="dist/reveal.css">
+		<link rel="stylesheet" href="dist/theme/simple.css" id="theme">
+        
+        <style>
+            .slides img{
+                height:70vh;
+            }
+        </style>
+	</head>
+	<body>
+        
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Prep
+$web = new \Spekulatius\PHPScraper\PHPScraper;
+$web->go('https://www.loesje.nl/posters');
+
+?>
+
+
+
+		<div class="reveal">
+
+			<div class="slides">
+				<section data-transition="fade-in slide-out" data-background-color="#ffe182">
+					
+                    <?php echo '<img src="'.$web->images[1].'" />';
+                    
+                    ?>
+				</section>
+                <section data-transition="fade-in slide-out" data-background-color="#bfdaa2">
+					
+                    <?php echo '<img src="'.$web->images[2].'" />';
+                    
+                    ?>
+				</section>
+                <section data-transition="fade-in slide-out" data-background-color="#F6B1A1">
+					
+                    <?php echo '<img src="'.$web->images[3].'" />';
+                    
+                    ?>
+				</section>
+                <section data-transition="fade-in slide-out" data-background-color="#ffe182">
+					
+                    <?php echo '<img src="'.$web->images[4].'" />';
+                    
+                    ?>
+				</section>
+                <section data-transition="fade-in slide-out" data-background-color="#bfdaa2">
+					
+                    <?php echo '<img src="'.$web->images[5].'" />';
+                    
+                    ?>
+				</section>
+                <section data-transition="fade-in slide-out" data-background-color="#F6B1A1">
+					
+                    <?php echo '<img src="'.$web->images[6].'" />';
+                    
+                    ?>
+				</section>
+			</div>
+
+		</div>
+
+		<script src="../dist/reveal.js"></script>
+		<script src="../plugin/highlight/highlight.js"></script>
+		<script>
+			Reveal.initialize({
+				center: true,
+                autoSlide: 5000,
+                loop: true,
+				hash: false,
+                controls: false,
+				// plugins: [ RevealHighlight ]
+			});
+		</script>
+
+	</body>
+</html>
+
+
